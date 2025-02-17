b96689e7dcf076abba9cf14b93b5afc93f06088d
= <sepehrinreallife@gmail.com>
2025-02-17 14:11:28 +0330

    style: change guideline colors

--- src/components/Legend.tsx
+++ src/components/Legend.tsx
@@ -1,24 +1,19 @@
 const Legend = () => {
 	return (
-		<>
-			<div className="bg-white rounded-lg px-4 py-2 border mt-6">
-				Index found!
+		<div className="flex flex-row gap-4">
+			<div className="flex items-center gap-1">
+				<div className="w-3 h-3 rounded-full bg-indigo-500"></div>
+				<span className="text-sm">Middle Element</span>
 			</div>
-			<div className="flex flex-row gap-4">
-				<div className="flex items-center gap-1">
-					<div className="w-3 h-3 rounded-full bg-orange-500"></div>
-					<span className="text-sm">Middle Element</span>
-				</div>
-				<div className="flex items-center gap-1">
-					<div className="w-3 h-3 rounded-full bg-lime-500"></div>
-					<span className="text-sm">Search range</span>
-				</div>
-				<div className="flex items-center gap-1">
-					<div className="w-3 h-3 rounded-full bg-teal-500"></div>
-					<span className="text-sm">Found Element</span>
-				</div>
+			<div className="flex items-center gap-1">
+				<div className="w-3 h-3 rounded-full bg-indigo-200"></div>
+				<span className="text-sm">Search range</span>
 			</div>
-		</>
+			<div className="flex items-center gap-1">
+				<div className="w-3 h-3 rounded-full bg-emerald-800"></div>
+				<span className="text-sm">Found Element</span>
+			</div>
+		</div>
 	);
 };
 
